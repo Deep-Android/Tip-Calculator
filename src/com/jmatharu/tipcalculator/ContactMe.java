755633f3fd31340951ba8a55c50e5a0b7319637f
JMatharu <jagdeepmatharu88@gmail.com>
2014-12-27 05:04:58 -0500

    Action bar/Background

---
+++ src/com/jmatharu/tipcalculator/ContactMe.java
@@ -0,0 +1,14 @@
+package com.jmatharu.tipcalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ContactMe extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_contact_me);
+	}
+
+}
